361fe7cbd57360b803dcf5559d6542d960c99b8f
SeohyunJeong95 <43589861+SeohyunJeong95@users.noreply.github.com>
2019-04-03 19:52:42 -0400

    Update GuiTester.java
    gui test

--- TalkBox/src/Test/GuiTester.java
+++ TalkBox/src/Test/GuiTester.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.*;
 import controller.Controller;
 import gui.AudioSelectionPanel;
 import gui.MainFrame;
+import gui.RecordDialog;
+import gui.ToolBar;
 import model.TalkBox;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.LinkedList;
@@ -25,41 +27,56 @@ public class GuiTester {
 	        frame.showIt();
 	    
 	}
-//	@Test
-//    public void testCustomCheckboxtest() throws InterruptedException {
-//		Thread.sleep(1000);
-//        AudioSelectionPanel audioselectionpanel = frame.getAUdioSelectionpanel();
-//        audioselectionpanel.checkBox.doClick();
-//        assertEquals(true,audioselectionpanel.checkBox.isSelected());
-//        Thread.sleep(1000);
-//    }
-//	@Test
-//    public void testSelectButton() throws InterruptedException {
-//		Thread.sleep(1000);
-//        AudioSelectionPanel audioselectionpanel = frame.getAUdioSelectionpanel();
-//        audioselectionpanel.checkBox.doClick();
-//        assertEquals(audioselectionpanel.setButton2.getText(),"Add Alright.wav as button 1");
-//        Thread.sleep(1000);
-//    }
+	@Test
+    public void testCustomCheckboxtest() throws InterruptedException {
+		Thread.sleep(1000);
+        AudioSelectionPanel audioselectionpanel = frame.getAUdioSelectionpanel();
+        audioselectionpanel.checkBox.doClick();
+        assertEquals(true,audioselectionpanel.checkBox.isSelected());
+        Thread.sleep(1000);    }
 	
-//	@Test
-//    public void testSelectButton2() throws InterruptedException {
-//		Thread.sleep(1000);
-//        AudioSelectionPanel audioselectionpanel = frame.getAUdioSelectionpanel();
-//        audioselectionpanel.checkBox.doClick();
-//        audioselectionpanel.audioData.setSelectedIndex(3);
-//        
-//        Thread.sleep(2000);
-//    }
+	@Test
+    public void testSelectButton() throws InterruptedException {
+		Thread.sleep(1000);
+        AudioSelectionPanel audioselectionpanel = frame.getAUdioSelectionpanel();
+        audioselectionpanel.checkBox.doClick();
+        assertEquals(audioselectionpanel.setButton2.getText(),"Add Alright.wav as button 1");
+        Thread.sleep(1000);
+    }
 	
-//	@Test
-//    public void testAudioNameTextField() throws InterruptedException {
-//        Thread.sleep(1000);
-//        AudioSelectionPanel audioselectionpanel = frame.getAUdioSelectionpanel();
-//        assertEquals(audioselectionpanel.audioSetName.getText(),"");
-//        audioselectionpanel.audioSetName.setText("emotion");
-//        assertEquals(audioselectionpanel.audioSetName.getText(),"emotion");
-//        Thread.sleep(1000);
-//    }
+	@Test
+    public void testSearchTextField() throws InterruptedException {
+		Thread.sleep(1000);
+        AudioSelectionPanel audioselectionpanel = frame.getAUdioSelectionpanel();
+        audioselectionpanel.checkBox.doClick();
+        audioselectionpanel.audioData.setSelectedIndex(3);
+        audioselectionpanel.searchAudio.setText("cool");
+        List<String> test = new LinkedList<>();
+        test.add("Cool.wav");
+        assertEquals(audioselectionpanel.audioData.getModel().toString(),test.toString());
+        Thread.sleep(2000);
+    }
 	
+	@Test
+    public void testAudioNameTextField() throws InterruptedException {
+        Thread.sleep(1000);
+        AudioSelectionPanel audioselectionpanel = frame.getAUdioSelectionpanel();
+        assertEquals(audioselectionpanel.audioSetName.getText(),"");
+        audioselectionpanel.audioSetName.setText("emotion");
+        assertEquals(audioselectionpanel.audioSetName.getText(),"emotion");
+        Thread.sleep(1000);
+    }
+
+   @Test
+    public void testRecordTextField() throws InterruptedException {
+      Thread.sleep(1000);
+      RecordDialog record = frame.getRecordDialog();
+      ToolBar tool = frame.getToolBar();
+      tool.recordButton.doClick();
+      assertEquals(record.audioFileName.getText(),"");
+      record.audioFileName.setText("LOL");
+      assertEquals(record.audioFileName.getText(),"LOL");
+      Thread.sleep(1000);
+  }
+
 }
