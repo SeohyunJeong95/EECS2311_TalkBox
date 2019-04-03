e63c524943168ff433dd048b45377d0cbcabca68
SeohyunJeong95 <43589861+SeohyunJeong95@users.noreply.github.com>
2019-04-03 16:59:04 -0400

    Update Tester.java
    updated tester

--- TalkBox/src/Test/Tester.java
+++ TalkBox/src/Test/Tester.java
@@ -31,15 +31,18 @@ public class Tester {
 	
 	public List<String> setupTestSet() {
 		List<String> testSet = new LinkedList<>();
-		testSet.add("hi.wav");
-		testSet.add("goodbye.wav");
-		testSet.add("excuseme.wav");
-		testSet.add("time.wav");
-		testSet.add("thankyou.wav");
-		
+		  testSet.add("hi.wav");
+		  testSet.add("goodbye.wav");
+		  testSet.add("excuseme.wav");
+		  testSet.add("time.wav");
+		  testSet.add("tired.wav");
+		  testSet.add("Yes.wav");
+		  testSet.add("Cool.wav");
+		  testSet.add("BONJOUR.wav");
+		  testSet.add("Alright.wav");
+		  testSet.add("No.wav");
 		return testSet;
 	}
-	
 	@Test
 	public void test_model_getAudioList() {
 		setup();
@@ -58,7 +61,7 @@ public class Tester {
 		setup();
 		List<String> tester = setupTestSet();
 		setupTestSet();
-		assertEquals(4, talkbox.getNumberOfAudioSets());
+		assertEquals(1, talkbox.getNumberOfAudioSets());
 	}
 	
 	@Test
@@ -67,19 +70,21 @@ public class Tester {
 		List<String> tester = setupTestSet();
 		setupTestSet();
 		talkbox.setAudioFileNames(tester);
-		assertEquals(5, talkbox.getNumberOfAudioSets(), "audioFileNames did not grow when set was added");
-		assertEquals(true, talkbox.getAudioList(4).contains("hi.wav"), "audioFileNames should contain hi.wav at the last index");
+		assertEquals(2, talkbox.getNumberOfAudioSets(), "audioFileNames did not grow when set was added");
+		assertEquals(true, talkbox.getAudioList(1).contains("hi.wav"), "audioFileNames should contain hi.wav at the last index");
 	}
 	
 	@Test
 	public void test_controller_addAudioSet() {
 		Controller controller = new Controller();
 		List<String> tester = new LinkedList<>();
+		tester.add("hi.wav");
+		tester.add("time.wav");
 		int oldCount = controller.getNumberOfAudioSets();
 		controller.addAudioSet(tester);
 		assertEquals(oldCount + 1, controller.getNumberOfAudioSets(), 
 				"controller did not grow audioFileNames");
-		assertEquals(tester, controller.getAudioList(4),
+		assertEquals(tester, controller.getAudioList(1),
 				"controller did not add tester successfully");	
 	}
 	
@@ -103,6 +108,24 @@ public class Tester {
 	public void test_controller_getNumberOfButtons() {
 		Controller controller = new Controller();
 		assertEquals(5, controller.getNumberOfButtons());
+	}
+	@Test
+	public void test_model_AddIcon() {
+		setup();
+		LinkedList<String> iconlist = setupIconTestSet();
+		talkbox.addIconbtn(iconlist);
+		assertEquals(iconlist, talkbox.getAudioIconDatabyidx(1));
+	}
+	
+	@Test
+	public void test_controller_RemoveIcon() {
+		setup();
+		LinkedList<String> iconlist = setupIconTestSet();
+		Controller controller = new Controller();
+		controller.addIconBtn(iconlist);
+		controller.removeIcon(1,   0);
+    	assertEquals(false, controller.getAudioIconDatabyidx(1).contains("happy.png"),
+			"happy.png should have been removed");
 	}
 	
 	/*
